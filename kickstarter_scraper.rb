52918e96b3e78ade3027271c44496718ef45a624
Jc Dior <jessicaclairdior@gmail.com>
2020-09-30 00:44:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,9 +2,6 @@
 require 'nokogiri'
 require 'pry'
 
-html = File.read('fixtures/kickstarter.html')
-
-kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
